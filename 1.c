6f0d0ac06bf86b8a0e823565825b4deaa876619a
BadaHong <110375619+BadaHong@users.noreply.github.com>
2022-09-12 14:25:03 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct{
+        double x;
+        double y;
+} Point;
+
+typedef struct{
+        char name[7];
+        Point points[3];
+} Person;
+
+int main(void){
+        Person students[3];
+        for(int i = 0; i < 3; i++){
+                scanf("%s %lf %lf %lf %lf %lf %lf", students[i].name, &students[i].points[0].x, &students[i].points[0].y, &students[i].points[1].x, &students[i].points[1].y, &students[i].points[2].x, &students[i].points[2].y);
+        }
+        for(int j = 0; j < 3; j++){
+                int len;
+                int list[3];
+                for(int k = 0; k < 3; k++){
+                        len = sqrt(pow(abs(students[j].points[k].x), 2) + pow(abs(students[j].points[k].y), 2));
+                        list[k] = len;
+                }
+                int max = 0;
+                for(int a = 0; a < 3; a++){
+                        if(list[max] < list[a]){
+                                max = a;
+                        }
+                }
+		//using "%g" to print double type numbers without useless 0s
+                printf("%s (%g, %g)\n", students[j].name, students[j].points[max].x, students[j].points[max].y);
+        }
+        return 0;
+}
