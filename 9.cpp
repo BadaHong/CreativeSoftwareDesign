c9d1108b172e1e8b9024c8c82d7607a3415cbf4d
BadaHong <110375619+BadaHong@users.noreply.github.com>
2022-09-25 19:51:24 +0900

    Add files via upload

--- 9.cpp
+++ 9.cpp
@@ -5,61 +5,59 @@
 using namespace std;
 
 void magicSquare(int**& arr, int N){
-	int a1[N][N], a2[N][N];
-	int num1 = 1, num2 = N*N;
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			a1[i][j] = num1++;
-			a2[i][j] = num2--;
-		}
-	}
-	inr r1, r2;
-	r1 = N/4;
-	r2 = N/2;
-	int row = 0; col = 0;
-	while(row < N && col < N){
-		for(int row = 0; row < N; row++){
-			if((row >= 0 && row < r1 || (row > r1+r2 && row < r1+r2+r1)){
-				for(int col = 0; col < N; col++){
-					if((col >= 0 && col < r1) || (col > r1+r2 && col < r1+r2+r1)){
-						arr[row][col] = a1[row][col];
-					}
-					else{
-						arr[row][col] = a2[row][col];
-					}
-					col++;
-				}
-			}
-			else{
-				for(int col = 0; col < N; col++){
-                                        if((col >= 0 && col < r1) || (col > r1+r2 && col < r1+r2+r1)){
-                                                arr[row][col] = a1[row][col];
-                                        }
-                                        else{
-                                                arr[row][col] = a2[row][col];
-                                        }
-                                        col++;
+        int a1[N][N], a2[N][N];
+        int num1 = 1, num2 = N*N;
+        for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                        a1[i][j] = num1++;
+                        a2[i][j] = num2--;
+                }
+        }
+        int r1 = N/4, r2 = N/2;
+        for(int row = 0; row < N; row++){
+                if((row >= 0 && row < r1) || (row >= r1+r2 && row < N)){
+                        for(int col = 0; col < N; col++){
+                                if((col >= 0 && col < r1) || (col >= r1+r2 && col < N)){
+                                        arr[row][col] = a1[row][col];
                                 }
-			}
-		}
-	}
+                                else{
+                                        arr[row][col] = a2[row][col];
+                                }
+                        }
+                }
+                else{
+                        for(int col = 0; col < N; col++){
+                                if((col >= 0 && col < r1) || (col >= r1+r2 && col < N)){
+                                        arr[row][col] = a2[row][col];
+                                }
+                                else{
+                                        arr[row][col] = a1[row][col];
+                                }
+                        }
+                }
+        }
 }
+
 int main(){
-	int N;
-	cin >> N;
-	if(N % 4 != 0){
-		return 0;
-	}
-	int** arr = new int*[N];
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-	for(int i = 0; i < N; i++){
-		delete[] arr[i];
-	}
-	delete[] arr;
-	return 0;
+        int N;
+        cin >> N;
+        if(N % 4 != 0){
+                return 0;
+        }
+        int** arr = new int*[N];
+        for(int i = 0; i < N; i++){
+                arr[i] = new int[N];
+        }
+        magicSquare(arr, N);
+        for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                        cout << arr[i][j] << " ";
+                }
+                cout << endl;
+        }
+        for(int i = 0; i < N; i++){
+                delete[] arr[i];
+        }
+        delete[] arr;
+        return 0;
 }
