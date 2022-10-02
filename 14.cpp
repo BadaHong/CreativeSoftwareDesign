27278ec8ea5b625c1c385a8ab6b9e402932968a2
BadaHong <110375619+BadaHong@users.noreply.github.com>
2022-10-02 21:18:27 +0900

    Add files via upload

--- 14.cpp
+++ 14.cpp
@@ -1,8 +1,16 @@
+//this program will swap the values of integers and strings by using functions. function named swapInt is declared and described as inline function.
+
 #include <iostream>
 #include <string>
 #include "swap.h"
 
 using namespace std;
+
+inline void swapInt(int& n1, int& n2){
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+}
 
 void swapString(string& s1, string& s2){
         string temp = s1;
